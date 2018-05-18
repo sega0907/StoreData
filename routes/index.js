c2d60ee382773c6be18c4c202bbd81a0b46ceee3
sega0907 <sgarcia83@horizon.csueastbay.edu>
2018-05-17 22:13:22 -0700

    st

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
-var router = express.Router();
+var XXX = express.Router();
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sega0907:0jamajodoremi@ds051831.mlab.com:51831/heroku_jw552rkk';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+XXX.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+        //get handle to the database
+        var theDatabase = client.db('heroku_jw552rkk');
+
+
+        //get collection of Routes
+        var Routes = db.collection('Routes');
+        //get all Routes
+        Routes.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('pages/mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+
+});//end XXX.get
