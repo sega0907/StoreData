48d53aabd3e15b1746860f05fd2d6ad54281a356
sega0907 <sgarcia83@horizon.csueastbay.edu>
2018-05-23 15:51:21 -0700

    st

--- controllers/database.js
+++ controllers/database.js
@@ -1,59 +1,86 @@
-var mongodb = require('mongodb');
+var mongodb = require('mongodb').mongodb;
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sega0907:0jamajodoremi@ds051831.mlab.com:51831/heroku_jw552rkk';
+var express = require('storeData');
+var router = storeData.Router();
 
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+//########################################
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-/** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
- * queries the Routes collection to retrieve all the routes and build the output usig the
- * ejs template mongodb.ejs found in views directory
- * @param request
- * @param response
- *
- */
+router.post('/storeData', function(req, res, next)
+{
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    var firstname = req.body.billfirstname;
+    var lastname = req.body.billlastname; //to work with separtors on any OS including Windows
+    var street = req.body.'"" + billaddress1 + billaddress2'; //for use in GET Query string of form URI/path?name=value
+    var city = req.body.billcity;
+    var bstate = req.body.billstate; //to work with separtors on any OS including Windows
+    var zip = req.body.billzip; //for use in GET Query string of form URI/path?name=value
+    var email = req.body.billemail;
+    var cardtype = req.body.cardtype; //to work with separtors on any OS including Windows
+    var cardnumber = req.body.cardnumber; //for use in GET Query string of form URI/path?name=value
+    var cardexp = req.body.'"" + cardexpmonth + "/" cardexpyear';
+    var cardcvv = req.body.cardcvv; //to work with separtors on any OS including Windows
+    var shipstreet = req.body.'"" + shipaddress1 + shipaddress2)'; //for use in GET Query string of form URI/path?name=value
+    var shipcity = req.body.shipcity;
+    var shipstate = req.body.shipstate; //to work with separtors on any OS including Windows
+    var shipzip = req.body.shipzip; //for use in GET Query string of form URI/path?name=value
+    var order = req.body.order;
+    var total = req.body.total; //for use in GET Query string of form URI/p
+});
 module.exports.getAllOrders =  function (request, response) {
 
-    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+    mongodb.MongoClient.connect('mongodb://sega0907:0jamajodoremi@ds051831.mlab.com:51831/heroku_jw552rkk', function(err, db) {
         if(err) throw err;
+        // var theDatabase = client.db('heroku_jw552rkk');
 
+        var dbo = db.db('heroku_jw552rkk');
+            var myobj =
+                { _id: getRandomArbitrary(10000, 20000),
+                    FIRSTNAME: firstname, LASTNAME: lastname,
+                    STREET: street, CITY: city, STATE: bstate,
+                    ZIP: zip, EMAIL: email};
 
-        //get handle to the databse
-        var theDatabase = client.db('heroku_jw552rkk');
+            dbo.collection("CUSTOMERS").insertOne(myobj, function(err, res) {
+                if (err) throw err; });
 
+        var dbi = db.db('heroku_jw552rkk');
+            var myobj1 =
+                {_id: getRandomArbitrary(20001, 30000),
+                CUSTOMER_ID: myobj._id, CREDITCARDTYPE: cardtype,
+                CREDITCARDNUM: cardnumber, CREDITCARDEXP: cardexp,
+                CREDITCARDSECURITYNUM: cardcvv};
+            dbi.collection("BILLING").insertOne(myobj1, function(err, res) {
+                if (err) throw err; });
 
-        //get collection of routes
-        var orders = theDatabase.collection('ORDERS');
-
-
-        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        var c = orders.find({});
-
-        c.forEach(
-            function(myDoc) {
-                console.log( "sega0907: " + myDoc.name );  //just  loging the output to the console
+        var dba = db.db('heroku_jw552rkk');
+            var myobj2 =
+                {_id: getRandomArbitrary(30001, 40000),
+                    CUSTOMER_ID: myobj._id, SHIPPING_STREET: shipstreet,
+                    SHIPPING_CITY: shipcity, SHIPPING_STATE: shipstate,
+                    SHIPPING_ZIP: shipzip
+                };
             }
-        );
+            dbo.collection("SHIPPING").insertOne(myobj2, function(err, res)
+            { if (err) throw err; });
 
+    var d = new Date(jsonDate);
+        var dbc = db.db('heroku_jw552rkk');
+            var myobj3 =
+                {_id: getRandomArbitrary(40001, 50000),
+                    CUSTOMER_ID: myobj._id, BILLING_ID: myobj1._id,
+                    SHIPPING_ID: myobj2._id, DATE: d,
+                    PRODUCT_VECTOR: order, ORDER_TOTAL: total};
+            dbc.collection("ORDERS").insertOne(myObj3, function(err, res)
+            { if (err) throw err; });
 
-        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
-        //   array called docs that you  forward to the  getAllRoutes.ejs view for use there
-        Orders.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('getAllOrders', {results: docs});
-
-        });
-
-
-        //Showing in comments here some alternative read (find) requests
-        //this gets Routes where frequency>=10 and sorts by name
-        // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Routes by name
-        //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
-
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
+            db.close();
+    };//end of connect
 };//end function
